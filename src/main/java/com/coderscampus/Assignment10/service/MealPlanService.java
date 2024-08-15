25741e298e5f69ec68a53ed51c4f451586cf1f23
nayelibueso <nayelibueso50@gmail.com>
2024-08-14 22:38:11 -0500

    Created  RestTemplate in each method to make a HTTP GET request to a public API and handle the response.

--- src/main/java/com/coderscampus/Assignment10/service/MealPlanService.java
+++ src/main/java/com/coderscampus/Assignment10/service/MealPlanService.java
@@ -1,8 +1,46 @@
 package com.coderscampus.Assignment10.service;
 
+import java.net.URI;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.coderscampus.Assignment10.domain.DayResponse;
+import com.coderscampus.Assignment10.domain.WeekResponse;
 //Handle the logic for calling the Spoontacular API and process the responses.
 @Service
 public class MealPlanService {
-
+	
+	private static final String API_BASE_URL = "https://api.spoonacular.com/mealplanner/generate";
+	private static final String API_KEY = "1c039f06ddb0400d9c0d5034a2e09597";
+	
+	private WeekResponse getWeekMeals (String numCalories, String diet, String exclusions ) {
+		URI uri = UriComponentsBuilder.fromHttpUrl(API_BASE_URL)
+										.queryParam("timeFrame", "week")
+										.queryParam("targetCalories", numCalories)
+										.queryParam("diet", diet)
+										.queryParam("exlude", exclusions)
+										.queryParam("apiKey", API_KEY)
+										.build()
+										.toUri();
+		RestTemplate rt = new RestTemplate();
+		WeekResponse response = rt.getForObject(uri, WeekResponse.class);
+		return response;
+		
+	}
+	
+	private DayResponse getDayMeals (String numCalories, String diet, String exclusions) {
+		URI uri = UriComponentsBuilder.fromHttpUrl(API_BASE_URL)
+										.queryParam("timeFrame", "day")
+										.queryParam("targetCalories", numCalories)
+										.queryParam("diet", diet)
+										.queryParam("exclude", exclusions)
+										.queryParam("apiKey", API_KEY)
+										.build()
+										.toUri();
+		RestTemplate rt = new RestTemplate();
+		DayResponse response = rt.getForObject(uri, DayResponse.class);
+		return response;
+	}
 }
--- src/main/java/com/coderscampus/Assignment10/web/SpoontacularController.java
+++ src/main/java/com/coderscampus/Assignment10/web/SpoontacularController.java
@@ -1,15 +1,25 @@
 package com.coderscampus.Assignment10.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coderscampus.Assignment10.domain.DayResponse;
 import com.coderscampus.Assignment10.domain.WeekResponse;
+import com.coderscampus.Assignment10.service.MealPlanService;
 
 //Class will handle requests related to the Spoontacular API
 @RestController
 public class SpoontacularController {
+	
+	private MealPlanService mealPlanService;
+	
+	@Autowired
+	public SpoontacularController (MealPlanService mealPlanService) {
+		this.mealPlanService = mealPlanService;
+	}
+	
 	@GetMapping("mealplanner/week")
 	public ResponseEntity<WeekResponse> getWeekMeals(String numCalories, String diet, String exclusions){
 		
